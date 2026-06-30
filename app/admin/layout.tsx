20a75c161403a95eb903e6369f05ed37f6b8da84
Mohamed Eeman (IT) <monk.eemoan@gmail.com>
2026-06-30 12:39:29 +0500

    Remove admin navbar fetch

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,6 @@
 import { AdminShell } from "@/components/admin-shell";
 import { ADMIN_LOGIN_PATH } from "@/lib/auth/bootstrap-admin";
 import { getCurrentAdminUser } from "@/lib/auth/require-admin";
-import { getNavbarContent } from "@/lib/site-content";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -20,11 +19,5 @@ export default async function AdminLayout({ children }: Readonly<{ children: Rea
     }
   }
 
-  const { content: navbar } = await getNavbarContent("published");
-
-  return (
-    <AdminShell logoUrl={navbar.whiteLogoUrl || navbar.primaryLogoUrl || navbar.blackLogoUrl}>
-      {children}
-    </AdminShell>
-  );
+  return <AdminShell>{children}</AdminShell>;
 }
